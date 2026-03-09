afcb11f1d438cdab735b40a16f9256d7814bfecf
davidcastanos <davidcastanosperez@gmail.com>
2026-03-09 10:57:46 +0100

    deberes

--- src/download.py
+++ src/download.py
@@ -21,47 +21,45 @@ headers = {
 url = "https://v3.football.api-sports.io/standings"
 
 os.makedirs('data', exist_ok=True)
-
-for nombre_liga, liga_id in ligas.items():
-    datos = {
-        'league': liga_id,
-        'season': temporada
-    }
-
-    response = requests.get(url, headers=headers, params=datos)
-    
-
-    data = response.json()
         
-    if data['response']:
-        clasificacion = data['response'][0]['league']['standings'][0]
+
             
-        filename = f"data/clasificacion_{nombre_liga}_{temporada}.csv"            
+filename = f"data/clasificacion_{temporada}.csv"            
         
-        with open(filename, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
+with open(filename, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
                 
+    writer.writerow([
+        'Liga', 'Posicion', 'Equipo', 'Puntos', 'Partidos_Jugados', 
+        'Ganados', 'Empatados', 'Perdidos', 'Goles a favor', 'Goles en contra', 'Diferencia de goles'
+    ])
+    for nombre_liga, liga_id in ligas.items():
+        datos = {
+            'league': liga_id,
+            'season': temporada
+        }
+
+        response = requests.get(url, headers=headers, params=datos)
+
+        data = response.json()
+
+        if data['response']:
+            clasificacion = data['response'][0]['league']['standings'][0]
+
+        for estadisticas_equipo in clasificacion:
             writer.writerow([
-                'Posicion', 'Equipo', 'Puntos', 'Partidos_Jugados', 
-                'Ganados', 'Empatados', 'Perdidos', 'Goles a favor', 'Goles en contra', 'Diferencia de goles'
+                estadisticas_equipo['rank'],
+                estadisticas_equipo['team']['name'],
+                estadisticas_equipo['points'],
+                estadisticas_equipo['all']['played'],
+                estadisticas_equipo['all']['win'],
+                estadisticas_equipo['all']['draw'],
+                estadisticas_equipo['all']['lose'],
+                estadisticas_equipo['all']['goals']['for'],
+                estadisticas_equipo['all']['goals']['against'],
+                estadisticas_equipo['goalsDiff']
             ])
-                
-            for estadisticas_equipo in clasificacion:
-                writer.writerow([
-                    estadisticas_equipo['rank'],
-                    estadisticas_equipo['team']['name'],
-                    estadisticas_equipo['points'],
-                    estadisticas_equipo['all']['played'],
-                    estadisticas_equipo['all']['win'],
-                    estadisticas_equipo['all']['draw'],
-                    estadisticas_equipo['all']['lose'],
-                    estadisticas_equipo['all']['goals']['for'],
-                    estadisticas_equipo['all']['goals']['against'],
-                    estadisticas_equipo['goalsDiff']
-                ])
 
-        print(f"Archivo guardado correctamente: {filename}")
-    else:
-        print(f" La API no devolvió datos para {nombre_liga} en el año {temporada}.")
-
+    print(f"Archivo guardado correctamente: {filename}")
+    
 
