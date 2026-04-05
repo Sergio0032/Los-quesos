38cb3b9e30fa620cd1164580746dc613b2474b9e
davidcastanos <davidcastanosperez@gmail.com>
2026-04-05 15:07:33 +0200

    app

--- web/app.py
+++ web/app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Futbol champagne")
+st.title("Futbol Champagne")
 
-st.subheader("Clasificacion grandes 5 ligas temprada 2024-2025")
+st.subheader("Clasificación grandes 5 ligas temporada 2024-2025")
 
-# 1. Guarda los datos en la memoria
 df = pd.read_csv("../data/clasificacion_2024.csv")
 
-# 2. Dibuja los datos en la pantalla
 st.dataframe(df)
-
-
